9663fde6ee54c3acff378a537c2d7129c9b4b1b8
Mark Tiddy <marktiddy@gmail.com>
2019-12-11 22:00:34 +0000

    Added API and Javascript Functions

--- js/script.js
+++ js/script.js
@@ -2,40 +2,96 @@ var lyricRepository = (function () {
 
   var apiUrl = 'https://api.lyrics.ovh/v1/';
 
-
-  var lyricObject = {
-    band: 'Blink 182',
-    title: 'Waggy',
-    lyrics: 'Watching your house shrink away in my rear-view mirror'
+  //API Call function
+  function getSongDetails(bandName, songName) {
+    $.ajax(`${apiUrl}${bandName}/${songName}`, { dataType: 'json' })
+      .then(function (responseJSON) {
+        loadContent(bandName, songName, responseJSON)
+      }).catch(function (error) {
+        var $parentGridItem = $('.main-content');
+        $parentGridItem.empty();
+        $parentGridItem.append($('<h2 class="lyrics-title">Error</h2>'));
+        $parentGridItem.append($('<p class="lyrics-content">We didn\'t find that song title. Try searching another</p>'));
+      });
   }
 
-  function getLyrics() {
-    return lyricObject;
-  }
-
-  function loadContent() {
-    //Load the lyrics
-    var lyrics = getLyrics();
+  function loadContent(bandName, songName, lyrics) {
 
     //Let's get our parent grid class first
     var $parentGridItem = $('.main-content');
 
+    //Empty the parent
+    $parentGridItem.empty();
+
     //Create a new element for the title
-    var $newTitle = $(`<h2 class="lyrics-title">${lyrics.band} - ${lyrics.title}</h2>`);
+    var $newTitle = $(`<h2 class="lyrics-title">${bandName} - ${songName}</h2>`);
 
     //Create the lyric item
-    var $newLyrics = $(`<p class="lyrics-content">${lyrics.lyrics}</p>`);
+    var lyrics = lyrics.lyrics;
+    var $newLyrics = $(`<p class="lyrics-content">${lyrics}</p>`);
 
     //Finally, let's append the content
     $parentGridItem.append($newTitle).append($newLyrics);
   }
 
+  //Add event listeners form our form
+  $('#lyric-search-submit').on('click', function (event) {
+    eventListenerAction();
+  });
+
+  $('#lyrics-form').on('keypress', function (event) {
+    if (event.which == 13) {
+      eventListenerAction();
+    }
+  });
+
+  //Event listener for clicking into form fields
+  $('input').click(function (event) {
+    var $input = $('input');
+    $input.prop("placeholder", "");
+  })
+
+  //Function to respond to event listeners
+  function eventListenerAction() {
+    event.preventDefault();
+    //Check for errors and see what the function returns
+    if (errorChecking()) {
+      getSongDetails($('#band-name').val(), $('#song-title').val());
+    }
+  }
+
+
+  //function to error check form
+  function errorChecking() {
+    var $bandField = $('#band-name');
+    var $songField = $('#song-title');
+    var $bandError = $('.band-error');
+    var $songError = $('.song-error');
+
+    //First check the band field
+    if ($bandField.val() != '' && $songField.val() != '') {
+      $bandError.fadeOut().hide();
+      $songError.fadeOut().hide();
+      //Both fields have content so we can run the API call by returning true
+      return true;
+    } else if ($bandField.val() == '' && $songField.val() == '') {
+      $bandError.fadeIn().show();
+      $songError.fadeIn().show();
+      return false;
+    } else if ($bandField.val() == '') {
+      $bandError.fadeIn().show();
+      $songError.fadeOut().hide();
+      return false;
+    } else {
+      $bandError.fadeOut().hide();
+      $songError.fadeIn().show();
+      return false;
+    }
+
+  }
+
 
   return {
-    getLyrics: getLyrics,
     loadContent: loadContent,
   }
 })();
-
-//Load the default lyrics
-lyricRepository.loadContent();
